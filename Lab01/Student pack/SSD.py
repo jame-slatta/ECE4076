1f71f0c27b2d469aecbc7ce24b65289349ec8d3f
jame-slatta <143096853+jame-slatta@users.noreply.github.com>
2025-04-07 14:16:10 +1000

    Useful SSD function

---
+++ Lab01/Student pack/SSD.py
@@ -0,0 +1,14 @@
+import numpy as np
+Im1P1 = [40,30,20,30,20,10, 20, 10, 0]
+Im1P2 = [25,40,25,25,30,25,25,25,25]
+Im2P1 = [25,25,15,25,15,10,15,10,0]
+Im2P2 = [20,20,20, 20,20,15, 20, 15, 5]
+
+def calculate_ssd(img1, img2):
+    """Computing the sum of squared differences (SSD) between two images."""
+
+    return np.sum((np.array(img1, dtype=np.float32) - np.array(img2, dtype=np.float32))**2)
+print(calculate_ssd(Im1P1, Im2P1))
+print(calculate_ssd(Im1P1, Im2P2))
+print(calculate_ssd(Im1P2, Im2P1))
+print(calculate_ssd(Im1P2, Im2P2))
